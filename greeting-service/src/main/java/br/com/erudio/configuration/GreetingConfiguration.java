1547f62ae724bcabea32795eea998c67e73614d1
Lourival <lourivalnaneto@gmail.com>
2021-06-30 23:35:44 -0300

    cambio service

--- greeting-service/src/main/java/br/com/erudio/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/br/com/erudio/configuration/GreetingConfiguration.java
@@ -1,14 +1,17 @@
 package br.com.erudio.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
+
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public @Data class GreetingConfiguration {
-	
+
 	private String greeting;
 	private String defaultValue;
-	
+
 }
